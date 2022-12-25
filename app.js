82696d4db943c862a36159c34929c0cd55ae2fb7
jazzyjpfeifer@gmail.com <jazzyjpfeifer@gmail.com>
2022-12-24 23:19:55 -0700

    Update app.js
    added cors config options

--- app.js
+++ app.js
@@ -7,20 +7,13 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+app.use(cors({
+  origin: '*'
+}));
+
 
 //Routes
  const events = require('./routes/events');
-
-
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-
-  res.json(customers);
-});
 
 app.use('/', events);
 
