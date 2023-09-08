ea69549576ebaf1df87498d3ae8d2b2f3b4b5118
justinpfeifer <jazzyjpfeifer@gmail.com>
2023-09-08 15:12:20 -0600

    Added serverless netifly function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3001;
+const serverless = require('serverless-http');
+
 
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -16,5 +18,11 @@ app.use(cors({
  const events = require('./routes/events');
 
 app.use('/', events);
+app.use('/.netlify/functions/server', events);  // path must route to lambda
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports.handler = serverless(app);
+
+module.exports = app;
+module.exports.handler = serverless(app);
